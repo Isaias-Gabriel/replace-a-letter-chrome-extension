93325c25364f4884220ad5ae4446d9f968b65b4b
Isaias-Gabriel <hyzegabriel@gmail.com>
2022-06-13 20:36:47 -0300

    stable version but without user entry

--- content.js
+++ content.js
@@ -9,15 +9,46 @@ function gotHere(logToPrint) {
     }
 }
 
+function replaceLetters(nodeList, searchValue, replaceValue) {
+
+    const searchValueRegExp = new RegExp(searchValue, "g");
+
+    for (nodeListElement of nodeList) {
+
+        if (nodeListElement !== null) {
+            if (nodeListElement.nodeType === 3) {
+                nodeListElement.nodeValue = nodeListElement.nodeValue.
+                    replace(searchValueRegExp, replaceValue);
+            }
+    
+            else if (nodeListElement.hasChildNodes()) {
+                const auxiliaryNodeListElement = nodeListElement.childNodes;
+    
+                replaceLetters(auxiliaryNodeListElement, searchValue, replaceValue);
+            }
+    
+            else {
+                if (nodeListElement.innerText !== "" &&
+                nodeListElement.innerText !== undefined
+                ) {
+                    nodeListElement.innerText = nodeListElement.nodeValue.
+                        replace(searchValueRegExp, replaceValue);
+                }
+            }
+        }
+    }
+}
+
+
+
 
 
 // gotHere();
 
 const body = document.getElementsByTagName('body')[0].childNodes;
 
-window.pageBody = body;
-
-console.log("a");
+replaceLetters(body, "a", "x");  
+replaceLetters(body, "e", "x"); 
 
 
 
